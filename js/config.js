0582b0f57728f7b18020a14840fe7b3092a8200b
doxa <genezx01@gmail.com>
2026-06-21 12:55:49 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -6,7 +6,7 @@
 ============================================================ */
 window.CONFIG = {
   // 1) Paste your Paystack PUBLIC key here (starts with pk_live_ or pk_test_)
-  PAYSTACK_PUBLIC_KEY: "pk_live_84864b7a3c351a69dce1f1c22a1a726b393aa222",
+  PAYSTACK_PUBLIC_KEY:"sk_live_6f54f64982cb7146b12c53bd7b4364248bd83854",
 
   // 2) Where the Netlify Functions live. Leave as-is for Netlify.
   //    (On Netlify, functions are served from /.netlify/functions)
